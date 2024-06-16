3e01f35e8c83373aec79daf2f54130f8a8876406
Stepan Zolotukhin <stepan.zolotukhin@yahoo.com>
2024-06-16 11:24:53 -0700

    Remove common code from the repo

--- game/heroes-of-crypto-common/src/index.ts
+++
@@ -1,8 +0,0 @@
-export * from "./grid/grid";
-export * as GridConstants from "./grid/grid_constants";
-export * as GridMath from "./grid/grid_math";
-export * from "./grid/grid_settings";
-export * from "./obstacles/obstacle_type";
-export * from "./units/unit_stats";
-export * as HoCLib from "./utils/lib";
-export * as HoCMath from "./utils/math";
--- game/heroes-of-crypto-common/src/obstacles/obstacle_type.ts
+++
@@ -1,6 +0,0 @@
-export enum ObstacleType {
-    BLOCK = -1,
-    HOLE = -2,
-    WATER = -3,
-    LAVA = -4,
-}
